f6b0d9e6ea3119197dd29b846b73aac4a211a1e8
PhenothX <tahshkhem@yahoo.com.sg>
2017-02-13 12:24:29 +0800

    Init

---
+++ contact.php
@@ -0,0 +1,39 @@
+<?php
+
+if($_POST["submit"]) {
+    
+  
+
+    $subject="Tahsh Khemlani Contact Form";
+    $name=$_POST["name"];
+    $email=$_POST["email"];
+    $message=$_POST["message"];
+    $check=$_POSt["check"];
+    
+
+
+    $mailBody=" Name: $name \n Message: $message \n Email: $email";
+    
+    if(empty($email)) {
+        
+        mail("phenothx@gmail.com", $subject, $mailBody, "From: phenothx@gmail.com");
+        main(sendto,subject,email,from);
+        
+    if(empty($check)) {
+    }
+    else
+    {
+         mail("phenothx@gmail.com", $subject, $mailBody, "From: $email");
+    }
+
+   
+        
+    
+    
+    header("Location: success.html");
+    exit;
+
+    
+}
+
+?>
