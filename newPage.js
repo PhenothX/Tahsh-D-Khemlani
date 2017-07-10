fbdc4ef0005507629af9263e34b79171e73d8519
Tahsh Khemlani <tahshkhem@yahoo.com.sg>
2017-07-10 15:33:22 +0800

    latest

---
+++ newPage.js
@@ -0,0 +1,22 @@
+function changePageHome()
+{
+  window.location.href = "./index.html";
+}
+
+
+function changePagePortraits()
+{
+  window.location.href = "./portraits.html";
+}
+
+
+function changePagePersonalAdventures()
+{
+  window.location.href = "./personaladventures.html";
+}
+
+
+function changePageContact()
+{
+  window.location.href = "./contact.html";
+}
